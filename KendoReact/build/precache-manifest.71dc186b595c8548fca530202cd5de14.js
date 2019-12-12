05edc00915204c4b93514ecfa9adaa7a02bcc841
Kanduri <srkanduri@deloitte.com>
2019-12-12 18:53:54 +0530

    Dashboard with chart features, pdf and exporter, suscribing dashboard feature using Kendo provided dependencies for React Application

---
+++ KendoReact/build/precache-manifest.71dc186b595c8548fca530202cd5de14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cd683ee29b98f6ef740",
+    "url": "/static/css/main.58e1a690.chunk.css"
+  },
+  {
+    "revision": "3cd683ee29b98f6ef740",
+    "url": "/static/js/main.bc95e286.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "93202751fb3cfa48a047",
+    "url": "/static/css/2.37a0ca8c.chunk.css"
+  },
+  {
+    "revision": "93202751fb3cfa48a047",
+    "url": "/static/js/2.abe1ffe8.chunk.js"
+  },
+  {
+    "revision": "c76b2afb7987b3a6a4e4423e3bfa00b8",
+    "url": "/index.html"
+  }
+];
---
+++ KendoReact/readme.md
@@ -0,0 +1,18 @@
+npm install
+npm start
+npm run build
+npm test
+---------------------
+Kendo React
+Bootstrapped with Create React App 
+Balsamiq for designing wireframes
+  Kendo React Packages:
+  1. Kendo Material Theme
+  2. KendoReact Layout Components
+  3. KendoReact PDF (Requires Kendo Drawing)
+  4. KendoReact Buttons
+  5. KendoReact Ripple (For ripple affect in Buttons)
+  6. KendoReact Dialog
+  7. Kendo React Charts(with HammerJs installed for chart events)
+Bootstrap 4 grid
+HTML, CSS, ReactJS
